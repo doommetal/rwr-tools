437c2abebb150e3ad3475b68201905be4be86731
DoomMetal <tacticaldoommetal@gmail.com>
2018-08-29 04:00:19 +0200

    Added saving support to Sproxel plugin
    Added rudimentary support for saving RWR voxel models. Still needs
    support for setting the coordinate offset.

--- sproxel/rwr_io.py
+++ sproxel/rwr_io.py
@@ -1,15 +1,16 @@
 import sproxel
-import os
+import os, sys
+
 
 plugin_info = dict(name='RWR import')
 
-
 def register():
     sproxel.register_importer(RWRImporter)
-
+    sproxel.register_exporter(RWRExporter)
 
 def unregister():
     sproxel.unregister_importer(RWRImporter)
+    sproxel.unregister_exporter(RWRExporter)
 
 
 class RWRImporter(object):
@@ -49,12 +50,13 @@ class RWRImporter(object):
                     # Put the data in a tuple and add it to the voxel list
                     voxels.append([x,y,z,(r,g,b,a)])
 
-
         # Sproxel does not support negative coordinates so we need to translate
         # them so they start at 0
         min_x = min(voxels,key=lambda item:item[0])[0]
         min_y = min(voxels,key=lambda item:item[1])[1]
         min_z = min(voxels,key=lambda item:item[2])[2]
+
+        # TODO: Save the offsets to a file. They will be useful when exporting
 
         for voxel in voxels:
             voxel[0] -= min_x
@@ -75,5 +77,59 @@ class RWRImporter(object):
         spr = sproxel.Sprite(layer)
         spr.name = os.path.splitext(os.path.basename(fn))[0]
         um.addSprite(prj, -1, spr)
+
+        return True
+
+
+class RWRExporter(object):
+    @staticmethod
+    def name(): return 'RWR model'
+
+    @staticmethod
+    def filter(): return '*.xml'
+
+    @staticmethod
+    def doExport(fn, prj, cur_sprite):
+        if not fn.lower().endswith('.xml') : fn+='.xml'
+
+        bounds = cur_sprite.bounds
+        sizex = bounds[1][0] - bounds[0][0] + 1
+        sizey = bounds[1][1] - bounds[0][1] + 1
+        sizez = bounds[1][2] - bounds[0][2] + 1
+
+        # TODO: Get the offset values
+
+        with open(fn, 'w') as f:
+            f.write('<model>\n')
+            f.write('    <voxels>\n')
+
+            # Write woxels to file
+            number_of_voxels = 0
+            for x in xrange(sizex):
+                for z in xrange(sizez):
+                    for y in xrange(sizey):
+                        r,g,b,a = cur_sprite.getColor(
+                            x + bounds[0][0],
+                            y + bounds[0][1],
+                            z + bounds[0][2])
+
+                        if a > 0:
+                            f.write('        <voxel x="' + str(x) +
+                                '" y="' + str(y) + '" z="' + str(z) +
+                                '" r="' + str(r) + '" g="' + str(g) +
+                                '" b="' + str(b) + '" a="' + str(a) + '" />\n')
+                            number_of_voxels += 1
+
+            f.write('    </voxels>\n')
+            
+            f.write('    <skeleton />\n')
+            f.write('    <skeletonVoxelBindings>\n')
+            f.write('        <group constraintIndex="0">\n')
+            for i in range(number_of_voxels):
+                f.write('            <voxel index="' + str(i) + '" />\n')
+            f.write('        </group>\n')
+            f.write('    </skeletonVoxelBindings>\n')
+
+            f.write('</model>\n')
 
         return True
